c316d37a4f28bb6909b0bf8c40836608eddf57c1
s.maksimyuk <s.maksimyuk@yandex.ru>
2026-03-16 13:39:31 +0300

    Main service: Fix checkstyle

--- stats/stats-client/src/main/java/ru/practicum/statsclient/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/statsclient/StatsClient.java
@@ -11,7 +11,6 @@ import ru.practicum.statsdto.EndpointHitDto;
 import ru.practicum.statsdto.ViewStatsDto;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
@@ -38,31 +37,27 @@ public class StatsClient {
                                        List<String> uris,
                                        boolean unique) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
-        try {
-            UriComponentsBuilder builder = UriComponentsBuilder
-                    .fromHttpUrl(baseUrl)
-                    .path("/stats")
-                    .queryParam("start", start.format(formatter))
-                    .queryParam("end", end.format(formatter))
-                    .queryParam("unique", unique);
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromHttpUrl(baseUrl)
+                .path("/stats")
+                .queryParam("start", start.format(formatter))
+                .queryParam("end", end.format(formatter))
+                .queryParam("unique", unique);
 
-            if (uris != null && !uris.isEmpty()) {
-                for (String u : uris) {
-                    builder.queryParam("uris", u);
-                }
+        if (uris != null && !uris.isEmpty()) {
+            for (String u : uris) {
+                builder.queryParam("uris", u);
             }
-
-            URI uri = builder.build().toUri();
-            ResponseEntity<ViewStatsDto[]> response =
-                    restTemplate.exchange(uri, HttpMethod.GET, null, ViewStatsDto[].class);
-            ViewStatsDto[] body = response.getBody();
-            if (body == null) {
-                return List.of();
-            }
-            return Arrays.asList(body);
-        } catch (URISyntaxException e) {
-            throw new IllegalArgumentException("Invalid stats service URL", e);
         }
+
+        URI uri = builder.build().toUri();
+        ResponseEntity<ViewStatsDto[]> response =
+                restTemplate.exchange(uri, HttpMethod.GET, null, ViewStatsDto[].class);
+        ViewStatsDto[] body = response.getBody();
+        if (body == null) {
+            return List.of();
+        }
+        return Arrays.asList(body);
     }
 
     // buildQuery больше не нужен
